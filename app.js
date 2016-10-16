509c5601b1821d81533737cccb52eb341d8c14b8
Aleksander-Bartosz <alex312@op.pl>
2016-10-16 14:11:23 +0200

    'ready'

---
+++ app.js
@@ -0,0 +1,69 @@
+
+$(document).ready(function () {
+    var row =$('.row');
+    var cout=1;
+    var slideChanger=$('.myButton');
+    var width= $(window).width();
+    var bgImg='url("style/img/large-photo';
+    function checkSize (width){
+            if (width>319 && width<480) {
+               bgImg='url("style/img/rlysmall-photo';
+                console.log('rlysmall');
+            }
+            else if (width<600 && width>480) {
+                bgImg='url("style/img/small-photo';
+                console.log('small');
+            }
+            else if (width>600 && width<720) {
+                bgImg='url("style/img/medium-photo';
+                console.log('medium');
+            }
+            else if (width>=720 && width<=1023) {  
+                bgImg='url("style/img/bigmedium-photo';
+                console.log('bigmedium');
+            }
+            else if (width>=1024 && width<1125) {
+                bgImg='url("style/img/semilarge-photo';
+                console.log('semilarge');
+            }
+            else if (width>=1125) {
+                bgImg='url("style/img/large-photo';
+                console.log('large');
+            }
+    }
+    checkSize(width);
+    $(window).on('resize', function () {
+        var resizeWidth= $(window).outerWidth();
+        checkSize(resizeWidth);
+        row.each(function(index, item) {
+            $(item).css('background-image',bgImg+cout+'.png")')
+        });
+        console.log(bgImg);
+        console.log(resizeWidth);
+    })
+    
+    slideChanger.on('click', function () {
+        var clickWidth= $(window).outerWidth();
+        checkSize(clickWidth);
+        if (cout<3){
+            cout++;
+        }
+        else {
+            cout=1;
+        }
+        var  tim = 30;
+        row.sort(function() { return 0.5 - Math.random() });
+        console.log(row)
+        row.each(function(index, item) {
+            tim=tim+60;
+            $(item).animate({
+               opacity:0.1  
+             },tim, function () {
+                 $(item).css('background-image',bgImg+cout+'.png")').animate({
+                     opacity:1
+                 },50);
+            });
+        });
+    });
+});
+       
